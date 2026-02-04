1f440cf2065c9b00593078e13f4b5c6e8964f946
vladiukdaria <vladiukdaria@gmail.com>
2026-02-04 09:25:52 +0100

    s

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,73 @@
 import pytest
+
 from app.restore_names import restore_names
 
-# write your tests here
+
+def test_restore_first_name_when_none():
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        }
+    ]
+
+    restore_names(users)
+
+    assert users[0]["first_name"] == "Jack"
+
+
+def test_restore_first_name_when_missing():
+    users = [
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        }
+    ]
+
+    restore_names(users)
+
+    assert users[0]["first_name"] == "Mike"
+
+
+def test_does_not_override_existing_first_name():
+    users = [
+        {
+            "first_name": "John",
+            "last_name": "Doe",
+            "full_name": "Jack Doe",
+        }
+    ]
+
+    restore_names(users)
+
+    assert users[0]["first_name"] == "John"
+
+
+def test_restore_names_for_list_of_users():
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+    restore_names(users)
+
+    assert users == [
+        {
+            "first_name": "Jack",
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "first_name": "Mike",
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]

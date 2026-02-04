9a2e4ef53cb45c023dc35732ec3ae8a246b02ff0
vladiukdaria <vladiukdaria@gmail.com>
2026-02-04 09:31:03 +0100

    ads

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,31 +1,36 @@
+import pytest
+
 from app.restore_names import restore_names
 
 
-def test_restore_first_name_when_none() -> None:
-    users = [
-        {
-            "first_name": None,
-            "last_name": "Holy",
-            "full_name": "Jack Holy",
-        }
-    ]
-
+@pytest.mark.parametrize(
+    "users, expected_first_name",
+    [
+        (
+            [
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy",
+                }
+            ],
+            "Jack",
+        ),
+        (
+            [
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams",
+                }
+            ],
+            "Mike",
+        ),
+    ],
+)
+def test_restore_first_name(users: list[dict], expected_first_name: str) -> None:
     restore_names(users)
 
-    assert users[0]["first_name"] == "Jack"
-
-
-def test_restore_first_name_when_missing() -> None:
-    users = [
-        {
-            "last_name": "Adams",
-            "full_name": "Mike Adams",
-        }
-    ]
-
-    restore_names(users)
-
-    assert users[0]["first_name"] == "Mike"
+    assert users[0]["first_name"] == expected_first_name
 
 
 def test_does_not_override_existing_first_name() -> None:
@@ -42,7 +47,7 @@ def test_does_not_override_existing_first_name() -> None:
     assert users[0]["first_name"] == "John"
 
 
-def test_restore_names_for_list_of_users() -> None:
+def test_restore_names_for_multiple_users() -> None:
     users = [
         {
             "first_name": None,
@@ -68,6 +73,7 @@ def test_restore_names_for_list_of_users() -> None:
             "last_name": "Adams",
             "full_name": "Mike Adams",
         },
+    ]
     ]        "last_name": "Adams",
             "full_name": "Mike Adams",
         },
